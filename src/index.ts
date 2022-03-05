3fe81d027e5b1e5a4d7df55ad86b78e263dd0833
Mario34 <zhl.mario34@outlook.com>
2022-03-05 11:34:14 +0800

    feat: 修改配置导入方式
    Closes #4

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import ci from 'miniprogram-ci'
 import minimist from 'minimist'
 import chalk from 'chalk'
-import path from 'path'
 import * as utils from './utils'
 
 import type { MiniProgramCI } from 'miniprogram-ci/dist/@types/types'
 
 const argv = minimist(process.argv.slice(2))
 const log = console.log
+const devLog = (...args) => {
+  if (argv.log) {
+    log(...args)
+  }
+}
 
 const checkConfig = (arg: Record<string, any>) => {
   const keys = ['appid', 'projectPath']
@@ -36,17 +40,13 @@ const tryGet = <T>(target: T | (() => T)) => {
   return target
 }
 
-(async () => {
-  const info = { git: utils.getLastCommit() }
-  const configPath = argv.c ?? path.resolve('mp-ci.config.js')
+export default (option) => {
+  const info = { git: utils.getGitInfo() }
   const {
-    default: {
-      config: setupConfig, setting: uploadSetting,
-      formatVersion, formatDesc,
-    },
-  } = await import(configPath)
+    config: setupConfig, setting: uploadSetting,
+    formatVersion, formatDesc,
+  } = option
 
-  console.log(await import(configPath))
   let config = tryGet<MiniProgramCI.IProject | null>(setupConfig)
   let setting = tryGet<MiniProgramCI.ICompileSettings | null>(uploadSetting)
 
@@ -60,10 +60,10 @@ const tryGet = <T>(target: T | (() => T)) => {
       es7: true,
     },
   }
-  log(config)
-  log(uploadConfig)
-  log(argv)
+  devLog(config)
+  devLog(uploadConfig)
+  devLog(argv)
 
   const project = new ci.Project(config)
   ci.upload({ project, ...uploadConfig })
-})()
+}
--- src/utils.ts
+++ src/utils.ts
@@ -3,40 +3,27 @@ import { execSync } from 'child_process'
 /**
  * 获取git提交信息
  */
-export function getLastCommit() {
+export function getGitInfo() {
   /**
-   * 格式同 git log --pretty=format
+   * git log --pretty=format
    * %ae - 邮箱
+   * %d - ref 名称
    * %s - 提交说明
    * %cd - 提交日期
+   * %ct - 提交日期
+   * %H - commit hash
+   * %h - 缩短的 commit hash
+   * %an - 作者名字
    */
-  const keyMap = ['%ae', '%s', '%cd', '%ct']
+  const keyMap = ['%ae', '%s', '%cd', '%ct', '%H', '%h', '%an', '%d']
   const valueMap = execSync(`git log -1 --pretty=format:${keyMap.toString().split(',')}`)
     .toString()
     .trim()
-    .toString()
     .split(',')
   const branch = execSync('git rev-parse --abbrev-ref HEAD')
   const info = keyMap.reduce((pre, cur, index) => {
     pre[cur] = valueMap[index]
     return pre
   }, {})
-  return `[分支]:${branch} [更新内容]:${info['%s']} [开发者]:${info['%ae']}`
-}
-
-/**
- * 获取版本对应机器人号码
- */
-export function getRobot(branch) {
-  if (/^release-.+$/.test(branch)) {
-    return 30
-  }
-  const valid = /^version-([1-9]\d|[1-9])((\.([1-9]\d|\d)){1,2}|(\.([1-9]\d|\d)){2}[^\s]+)$/.test(
-    branch,
-  )
-  const patch = branch.split('.').pop()
-  if (patch === '0') {
-    return 27
-  }
-  return valid ? patch : 28
+  return { ...info, branch: String(branch).trim() }
 }
